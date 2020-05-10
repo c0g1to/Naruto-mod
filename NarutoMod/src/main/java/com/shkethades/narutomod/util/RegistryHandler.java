36eb7b326ab341eaca33ecc51ca59771769933a8
Daniil <55106654+horriblehades@users.noreply.github.com>
2020-05-10 15:49:46 +0300

    ready

---
+++ NarutoMod/src/main/java/com/shkethades/narutomod/util/RegistryHandler.java
@@ -0,0 +1,20 @@
+package com.shkethades.narutomod.util;
+
+import com.shkethades.narutomod.NarutoMod;
+import com.shkethades.narutomod.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, NarutoMod.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static final RegistryObject<Item> SHURIKEN = ITEMS.register("shuriken", ItemBase::new);
+}
